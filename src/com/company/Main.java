363ad7ee1f8626096ffada16c502fb277b3b71ad
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-28 19:54:36 +0600

    Исправил многие ошибки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,22 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         double[] java = {-2.3, 6.5, -8.6, 9.3, -8.5, 3.6, -6.2, 1.5, -8.9, 3.6, -96.23, 6.8, -5.99, 2.99, -3.6};
-        int onePiece = 0;
-        double marsel = 0.0;
-        boolean window = false;
-        for (double android : java) {
-            if (android < 0) {
-                window = true;
+        double sum = 0.0;
+        int col = 0;
+        boolean isNegative = false;
 
-            }
-            else if (window){
-                onePiece++;
-                marsel += android;
+        for (double value:java) {
+            if (value < 0){
+                isNegative = true;
+            }else if (isNegative){
+                sum = sum + value;
+                col ++;
             }
         }
-        System.out.println("Среднее арифметичекое число = " + (marsel / onePiece));
+        double answer = sum / col;
+        System.out.println("Answer: " + answer);
     }
 }
